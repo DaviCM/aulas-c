e9dd05cb1fc5172309ecc735e814a35a85799a24
DaviCM <davimaciel2008@gmail.com>
2026-05-07 22:42:57 -0300

    feat: Começando a aprender ponteiros

---
+++ dia3/palestra.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Em geral, na computação, tipos de dados que ocupam mais de um byte, ou mais de um endereço de memória, têm
+seus bytes que o compõem em ordem. Isso é a "ordem dos bytes", em inglês endianess.
+Computadores podem ser big-endian (bytes em ordem com o mais significativo primeiro);
+little-endian (bytes em ordem com o mais significativo ao final);
+ou nenhuma organização em particular, com bytes dispersos. Acima disso, talvez eles nem
+sejam alocados em sequência na memória. */
+
+// Uma variável guarda um dado de um tipo específico, expresso na forma de bytes e armazenado na memória.
+// Um ponteiro é simplesmente uma variável que guarda o endereço onde esse dado está.
+
+// Ou seja: se temos um dado na memória, ele está em um endereço. O ponteiro é simplesmente uma variável que guarda esse endereço.
+// Ela não guarda o dado, mas nos diz onde ele está.
+// Será útil em chamadas de função.
+// endereço de memória == ponteiro
+
+void ptrs(void) {
+  int num = 42;
+  printf("O valor de nosso número é: %d \n", num);
+  printf("E na memória ele está em: %p \n", &num);
+  printf("E esse é um ponteiro para ele: %p \n", (void*)&num); // assim que se faz conversão de tipo no c, (tipo)var.
+                                                               // estamos convertendo esse ponteiro para um ponteiro void
+                                                               // ele é um "tipo sem tipo", então aceita qualquer quantidade de bytes.
+};
+
+int main (void) {
+    ptrs();
+    return 0;
+};
---
+++ dia3/pointers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+    int numero; // um int!
+    int *to_numero; // um ponteiro para um int, leva para o endereço de memória onde há um inteiro
+
+    to_numero = &numero; // o ponteiro agora guarda o valor do int que declaramos
+    
+    // aqui o C vai usar "sobrecarga de operador", então é pra prestar atenção
+
+    numero = 42;
+    *to_numero = 39;
+
+    printf("Nosso valor final é: %d \n", numero);
+    return 0;
+};
+
+/* 
+na declaração, * define o tipo int *, um ponteiro para um int
+nós igualamos esse ponteiro ao endereço de memória de uma variável int. Ele agora "referencia" numero
+Depois, nós atribuímos um valor à variável
+E depois usamos o indirection operator para "desreferenciar" o ponteiro
+Ou seja: utilizamos ele para acessar o valor do endereço de memória que o ponteiro guardava.
+Nesse caso o valor é o valor de numero, então nós alteramos ele diretamente.
+ */
+
+ // Estamos dizendo ao C para usar o valor na memória que o ponteiro está apontando, não o ponteiro em si.
